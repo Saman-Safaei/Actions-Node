f040593699f7a67c4c2f987b7969e9de0348f7ab
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-05 15:12:05 +0430

    UPGRADE FILE UPLOADER

--- utils/uploads/file_upload.js
+++ utils/uploads/file_upload.js
@@ -4,7 +4,13 @@ const fileMimetypes = ["image/jpg", "image/jpeg", "image/png"];
 const storage = multer.diskStorage({
     destination: "files/",
     filename: (req, file, cb) => {
-        const filename = file.originalname.replace(/\s/g, "_");
+        const currentDate = new Date('fa-IR');
+        const dateString = currentDate.getFullYear() + "_" 
+            + (currentDate.getMonth() + 1) + "_" 
+            + (currentDate.getDay() + 1) + "_" 
+            + (currentDate.getHours()) + currentDate.getMinutes() + currentDate.getSeconds();
+        
+        const filename = file.originalname.replace(/[^A-Za-z.]/g, "_") + dateString;
         cb(null, filename);
     }
 });
