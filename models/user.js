8c180bc3911d38f555286ee0a1b90191cfc4746e
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-15 15:05:30 +0430

    add verified boolean to user model

--- models/user.js
+++ models/user.js
@@ -1,25 +1,29 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = sequelize => {
-    sequelize.define("User", {
-        id: {
-            type: DataTypes.INTEGER(),
-            primaryKey: true,
-            autoIncrement: true
-        },
-        username: {
-            type: DataTypes.STRING(),
-            unique: true,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING(),
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING(),
-            unique: true,
-            allowNull: false
-        }
-    });
+  sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER(),
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING(),
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(),
+      unique: true,
+      allowNull: false
+    },
+    verified: {
+      type: DataTypes.BOOLEAN(),
+      defaultValue: false
+    }
+  });
 };
