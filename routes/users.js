576220f32a0dc5cd3373fb25d080221dc4abaa97
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-15 15:05:44 +0430

    add cAuth to routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { cLogin, cRegister } = require("../controllers/users");
+const { cLogin, cRegister, cAuth } = require("../controllers/users");
 
 router.post("/login", express.json(), cLogin);
 router.post("/register", express.json(), cRegister);
+router.get("/auth", cAuth);
 
 module.exports = router;
