c70bd0d7333cd3452db1331dae83243ebc804ef3
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-15 15:19:06 +0430

    revert mail_gen.js for use in localhost

--- utils/mail/mail_gen.js
+++ utils/mail/mail_gen.js
@@ -1,5 +1,5 @@
 module.exports = (user, hash) => {
   return "<h1>Actions</h1>" + 
     `<p>Welcome to our service dear ${user}.</p>` +
-    `<a href='${"https://api-actions-node.herokuapp.com/auth/?t=" + hash}'>Verify Your Email</a>`;
+    `<a href='${"http://localhost:3030/auth/?t=" + hash}'>Verify Your Email</a>`;
 }
