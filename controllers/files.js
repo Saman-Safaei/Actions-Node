07ab3349093953d0a6465cd538368bdca3d65aee
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-17 14:53:14 +0430

    use fs to find file and check  have user access to file

--- controllers/files.js
+++ controllers/files.js
@@ -1,3 +1,22 @@
+const fs = require("fs");
+
+const { relativePath } = require("../utils/relative_path");
 const db = require("../utils/database/database");
+
 const { User } = db.models;
 
+// Require Auth Middleware --- have a filename param
+module.exports.cSendFile = (req, res) => {
+    const user = req.user;
+    const filename = req.params.filename;
+    const filepath = relativePath("files", filename);
+
+    // check file exists or not
+    if (!fs.existsSync(filepath)) return res.status(404).json({ message: "File not found." });
+
+    const fileRelativeToUser = filename === (user.img); // is User access the file or not ?
+
+    if (!fileRelativeToUser) return res.status(401).json({ message: "You don't have access to this file." });
+
+    res.sendFile(filepath);
+};
