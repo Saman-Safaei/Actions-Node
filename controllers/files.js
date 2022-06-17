a9e095c70ad878a3db47940b3f847f44c24a85f6
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-06-17 16:27:27 +0430

    fix check file belong to user bug

--- controllers/files.js
+++ controllers/files.js
@@ -6,17 +6,17 @@ const db = require("../utils/database/database");
 const { User } = db.models;
 
 // Require ParamAuth Middleware --- have a filename param
-module.exports.cSendFile = (req, res) => {
-    const user = req.user;
-    const filename = req.params.filename;
-    const filepath = relativePath("files", filename);
+module.exports.cSendFile = async (req, res) => {
+  const user = req.user;
+  const filename = req.params.filename;
+  const filepath = relativePath("files", filename);
 
-    // check file exists or not
-    if (!fs.existsSync(filepath)) return res.status(404).json({ message: "File not found." });
+  // check file exists or not
+  if (!fs.existsSync(filepath)) return res.status(404).json({ message: "File not found." });
 
-    const fileRelativeToUser = filename === (user.img); // is User access the file or not ?
+  const fileRelativeToUser = await user.getActions({ attributes: ['img'], where: { img: filename } }); // is User have this file to any action or not ?
 
-    if (!fileRelativeToUser) return res.status(401).json({ message: "You don't have access to this file." });
+  if (!fileRelativeToUser[0]) return res.status(401).json({ message: "You don't have access to this file." });
 
-    res.sendFile(filepath);
+  res.sendFile(filepath);
 };
